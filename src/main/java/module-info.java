6558a88c0464d07631e544d8c4a89662ba5520e8
Yiğit Türk <yigit.turk@ug.bilkent.edu.tr>
2026-03-01 13:24:42 +0300

    The environment for client side and server side development is set. The basic application on the previous commit is preserved. Additionally, I have included a maven wrapper.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.evalit {
-    requires transitive javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.evalit to javafx.fxml;
-    exports com.evalit;
-}
