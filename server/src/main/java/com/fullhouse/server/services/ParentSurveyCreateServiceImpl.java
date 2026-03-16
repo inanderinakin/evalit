729e514b3526a4d63451e63d35a69f5afd07f63b
Yiğit Türk <yigit.turk@ug.bilkent.edu.tr>
2026-03-16 15:33:35 +0300

    ParentSurveyService is ready for database connection
    The ParentSurvey createSurvey method is ready for database integration. A change on the ParentSurveyEntity might be required.

--- server/src/main/java/com/fullhouse/server/services/ParentSurveyCreateServiceImpl.java
+++ server/src/main/java/com/fullhouse/server/services/ParentSurveyCreateServiceImpl.java
@@ -4,12 +4,19 @@ import com.fullhouse.DTOs.ParentSurveyCreateRequest;
 import com.fullhouse.DTOs.ParentSurveyCreateResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ParentSurveyCreateServiceImpl implements ParentSurveyCreateService{
 
 
     @Override
     public ParentSurveyCreateResponse createSurvey(ParentSurveyCreateRequest request) {
-        return new ParentSurveyCreateResponse("abc");
+        List<String> questions = request.getQuestions();
+
+        // TODO: Please create a new ParentSurvey and add it to the database.
+        //  Also, the ParentSurvey entity must maintain a question set too.
+
+        return new ParentSurveyCreateResponse();
     }
 }
