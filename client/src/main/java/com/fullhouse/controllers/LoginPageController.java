a7e806affce4913eee11175bc51258856f7d4337
İnan Derin Akın <inandakin@gmail.com>
2026-03-20 22:23:10 +0300

    Created new UI template pages like Apply Survey page, popups and etc. Also added a browser opening system so it is a start for Google Authentication.

--- client/src/main/java/com/fullhouse/controllers/LoginPageController.java
+++ client/src/main/java/com/fullhouse/controllers/LoginPageController.java
@@ -1,15 +1,24 @@
 package com.fullhouse.controllers;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import com.fullhouse.App;
 
 import javafx.fxml.FXML;
 
+import java.awt.Desktop;
+
 public class LoginPageController {
 
     @FXML
-    private void handleLogin() throws IOException {
+    private void handleLogin() throws IOException, URISyntaxException {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            desktop.browse(new URI("google.com"));
+        }
+        
         App.setRoot("homePage"); 
     }
 }
