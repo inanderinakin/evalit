d9fe61638e66f750610903cdeef788a428e08666
Yiğit Türk <yigit.turk@ug.bilkent.edu.tr>
2026-03-28 22:15:15 +0300

    Implemented the controller for ParentSurveyGetList

--- server/src/main/java/com/fullhouse/server/controllers/ParentSurveyGetListController.java
+++ server/src/main/java/com/fullhouse/server/controllers/ParentSurveyGetListController.java
@@ -1,5 +1,10 @@
 package com.fullhouse.server.controllers;
 
+import com.fullhouse.DTOs.ParentSurveyDTOs.ParentSurveyListRequest;
+import com.fullhouse.DTOs.ParentSurveyDTOs.ParentSurveyListResponse;
+import com.fullhouse.server.services.ParentSurveyService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("parent-survey/get-list")
 public class ParentSurveyGetListController {
+    private ParentSurveyService parentSurveyService;
 
+    public ParentSurveyGetListController(ParentSurveyService parentSurveyService) {
+        this.parentSurveyService = parentSurveyService;
+    }
+
+    @GetMapping
+    public ParentSurveyListResponse getParentSurveyOfUser(@RequestBody ParentSurveyListRequest request) {
+        return parentSurveyService.getParentSurveysOfUser(request);
+    }
 }
